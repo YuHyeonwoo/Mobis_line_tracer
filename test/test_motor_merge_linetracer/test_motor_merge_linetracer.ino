0df079d40a4184e8592211f3f3aa8ea7c5e33c7b
yhw <mansu9801@naver.com>
2023-02-23 14:58:33 +0900

    [TEST] | modifying merge

--- test/test_motor_merge_linetracer/test_motor_merge_linetracer.ino
+++ test/test_motor_merge_linetracer/test_motor_merge_linetracer.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 #include <AFMotor.h>
-#define end;
+//#define end;
 
 // motor driver
 AF_DCMotor motor1(1);
@@ -14,9 +14,9 @@ void setup() {
   Serial.begin(9600);
 
   // motor_Dirver
-  motor1.setSpeed(250);
+  motor1.setSpeed(200);
   motor1.run(RELEASE);
-  motor2.setSpeed(250);
+  motor2.setSpeed(200);
   motor2.run(RELEASE);
 
   // lineTracer
@@ -24,34 +24,50 @@ void setup() {
   pinMode(line_l, INPUT);   // 라인트레이서 센서는 '입력'
 }
 
+void Speed(int type, int speed){
+  if(type == 1){
+    motor1.run(RELEASE);
+    motor1.setSpeed(speed);
+  }
+  else if(type == 2){
+    motor2.run(RELEASE);
+    motor2.setSpeed(speed);
+  }
+  //delay(100);
+}
+
 void loop() {
 #ifndef end
   // put your main code here, to run repeatedly:
   int val_L = digitalRead(line_l);  // 센서 값을 읽어와서
   int val_R = digitalRead(line_r);  // 센서 값을 읽어와서
+  Serial.print("Left : ");
   Serial.println(val_L);
+  Serial.print("Right : ");
   Serial.println(val_R);
-
-  if (val_R == LOW && val_L == LOW){ // 둘다 검은색으로 들어올 때
+  
+  if (val_R == LOW && val_L == LOW){ // 둘다 검은색X
     motor1.run(FORWARD);
     motor2.run(FORWARD);
     //delay(500);
   }
   
-  if (val_R == LOW){     // 우회전
-    motor1.run(RELEASE);
+  else if (val_R == LOW){     // 왼쪽이 검은색
+    Speed(1,170);
+    motor1.run(BACKWARD);
     motor2.run(FORWARD);          
    //delay(500);
   }
-  else if (val_L == LOW){ // 좌회전
+  else if (val_L == LOW){ // 오른쪽이 검은색
     motor1.run(FORWARD);
-    motor2.run(RELEASE);
+    Speed(2,170);
+    motor2.run(BACKWARD);
     //delay(500);
   } 
-  else{               // 검정색 주행선을 벗어나면
-    motor1.run(FORWARD);
-    motor2.run(FORWARD);        // Buzzer에서 소리가 난다
-    //delay(500);
+  else{               // 둘다 검은색
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+    delay(200);
   }
 #endif
   
