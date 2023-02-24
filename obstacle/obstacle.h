0ec8c8f18f7510d9e44eaf82863d79f6f8fbaa0c
LeeYeongseok <aass05213@gmail.com>
2023-02-24 09:31:35 +0900

    obstacle header file

---
+++ obstacle/obstacle.h
@@ -0,0 +1,49 @@
+#ifndef __OBSTACLE_H__
+#define __OBSTACLE_H__
+
+int Buzzer_Pin = 10;
+int IR_Sensor_Pin = 9;
+int Tones[7] = {261, 294, 330, 349, 392, 440, 494};                   // 도, 레, 미, 파, 솔, 라, 시
+
+
+void startSound();
+void stopSound();
+void arriveSound();
+int checkOpstacle();
+
+void useBuzzerPin(){
+    pinMode(Buzzer_Pin, OUTPUT);
+}
+
+void useIRSensor(){
+    pinMode(IR_Sensor_Pin, INPUT);
+}
+
+int checkOpstacle(){
+  return !digitalRead(IR_Sensor_Pin);
+}
+
+void arriveSound(){
+    for(int i = 0; i<3; i++){
+      tone(Buzzer_Pin, Tones[0]);
+      delay(300);
+      tone(Buzzer_Pin, Tones[2]);
+      delay(300);
+      tone(Buzzer_Pin, Tones[4]);
+      delay(300);
+    }
+    tone(Buzzer_Pin, Tones[6]);
+    delay(300);
+    noTone(Buzzer_Pin);
+}
+
+void stopSound(){
+    for(int i = 0; i<3; i++){
+      tone(Buzzer_Pin, Tones[5]);
+      delay(300);
+      noTone(Buzzer_Pin);
+      delay(300);
+    }    
+}
+
+#endif
