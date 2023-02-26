f3c391e50f6a69d70d2cf28c9cefb2f65575449d
yhw <mansu9801@naver.com>
2023-02-26 12:35:35 +0900

    [TEST] | merge customarduino and LED

--- test/test_Register_LED/test_Register_LED.ino
+++ test/test_Register_LED/test_Register_LED.ino
@@ -1,12 +1,22 @@
+#include "customarduino.h"
+
 void setup() {
   // put your setup code here, to run once:
-  DDRD |= (1 << 7); // 7번 포트를 OUTPUT으로
+  // DDRD |= (1 << 7); // 7번 포트를 OUTPUT으로
+  pinMode_custom(3,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  PORTD |= (1 << 7); // 7번 포트에 전압 인가
-  delay(3000);
-  PORTD &= ~(1 << 7); // 7번 포트 전압 해제
-  delay(3000);
+  // PORTD |= (1 << 7); // 7번 포트에 전압 인가
+  for(int i=0; i<=250; i++){
+    analogWrite_custom(3,i);
+    delay(20);
+  }
+  for(int i=250; i>=0; i--){
+    analogWrite_custom(3,i);
+    delay(20);
+  }
+  // PORTD &= ~(1 << 7); // 7번 포트 전압 해제
+  // delay(3000);
 }
