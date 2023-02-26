d47e52fa78e0a3e154bfb4b4f78b9981e88448c3
LeeYeongseok <aass05213@gmail.com>
2023-02-26 12:42:43 +0900

    obstacle.h - arduino custom

--- main/obstacle.h
+++ main/obstacle.h
@@ -1,54 +1,36 @@
 #ifndef __OBSTACLE_H__
 #define __OBSTACLE_H__
 
-int Buzzer_Pin = 10;
-int IR_Sensor_Pin = 9;
+#include "customarduino.h"
+
 int Tones[7] = {261, 294, 330, 349, 392, 440, 494};                   // 도, 레, 미, 파, 솔, 라, 시
 
+class Obstacle{
+  int Buzzer_Pin;
+  int IR_Sensor_Pin;
+public:
+  void pinSet_custom(int Buzzer_Pin, int IR_Sensor_Pin); // 부저번호, 핀번호
+  int checkObstacle(); // 장애물이 인식되면 1, 아니면 0 출력
+  void stopSound(); // 경고음을 울림
+};
 
-void startSound();
-void stopSound();
-void arriveSound();
-int checkOpstacle();
-void startObstacleCheck();
-
-void useBuzzerPin(){
-    pinMode(Buzzer_Pin, OUTPUT);
+void Obstacle::pinSet(int Buzzer_Pin, int IR_Sensor_Pin){
+  this->Buzzer_Pin = Buzzer_Pin;
+  this->IR_Sensor_Pin = IR_Sensor_Pin;
+  pinMode_custom(this->Buzzer_Pin, OUTPUT);
+  pinMode_custom(this->IR_Sensor_Pin, INPUT);
 }
 
-void useIRSensor(){
-    pinMode(IR_Sensor_Pin, INPUT);
+int Obstacle::checkObstacle(){
+  return !digitalRead_custom(this->IR_Sensor_Pin);
 }
 
-void startObstacleCheck(){
-  if (checkOpstacle() == 1){
-    stopSound();
-  }
-}
-
-int checkOpstacle(){
-  return !digitalRead(IR_Sensor_Pin);
-}
-
-void arriveSound(){
-    for(int i = 0; i<3; i++){
-      tone(Buzzer_Pin, Tones[0]);
-      delay(300);
-      tone(Buzzer_Pin, Tones[2]);
-      delay(300);
-      tone(Buzzer_Pin, Tones[4]);
-      delay(300);
-    }
-    tone(Buzzer_Pin, Tones[6]);
-    delay(300);
-    noTone(Buzzer_Pin);
-}
-
-void stopSound(){
+void Obstacle::stopSound(){
     tone(Buzzer_Pin, Tones[5]);
     delay(300);
     noTone(Buzzer_Pin);
     delay(300);
 }
+
 
 #endif
